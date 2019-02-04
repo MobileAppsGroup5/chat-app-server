010c733d0d0ef765b31288dad70b5c786d0d344b
trungt19 <Trtt06070809!>
2019-02-04 12:54:21 -0800

    initial commit

---
+++ SetupTables.sql
@@ -0,0 +1,64 @@
+DROP TABLE IF EXISTS Members;
+CREATE TABLE Members (MemberID SERIAL PRIMARY KEY,
+                      FirstName VARCHAR(255) NOT NULL,
+		              LastName VARCHAR(255) NOT NULL,
+                      Username VARCHAR(255) NOT NULL UNIQUE,
+                      Email VARCHAR(255) NOT NULL UNIQUE,
+                      Password VARCHAR(255) NOT NULL,
+                      SALT VARCHAR(255),
+                      Verification INT DEFAULT 0
+);
+
+DROP TABLE IF EXISTS Contacts;
+CREATE TABLE Contacts(PrimaryKey SERIAL PRIMARY KEY,
+                      MemberID_A INT NOT NULL,
+                      MemberID_B INT NOT NULL,
+                      Verified INT DEFAULT 0,
+                      FOREIGN KEY(MemberID_A) REFERENCES Members(MemberID),
+                      FOREIGN KEY(MemberID_B) REFERENCES Members(MemberID)
+);
+
+DROP TABLE IF EXISTS Chats;
+CREATE TABLE Chats (ChatID SERIAL PRIMARY KEY,
+                    Name VARCHAR(255)
+);
+
+DROP TABLE IF EXISTS ChatMembers;
+CREATE TABLE ChatMembers (ChatID INT NOT NULL,
+                          MemberID INT NOT NULL,
+                          FOREIGN KEY(MemberID) REFERENCES Members(MemberID),
+                          FOREIGN KEY(ChatID) REFERENCES Chats(ChatID)
+);
+
+DROP TABLE IF EXISTS Messages;
+CREATE TABLE Messages (PrimaryKey SERIAL PRIMARY KEY,
+                       ChatID INT,
+                       Message VARCHAR(255),
+                       MemberID INT,
+                       FOREIGN KEY(MemberID) REFERENCES Members(MemberID),
+                       FOREIGN KEY(ChatID) REFERENCES Chats(ChatID),
+                       TimeStamp TIMESTAMP WITH TIME ZONE DEFAULT current_timestamp
+);
+
+DROP TABLE IF EXISTS Locations;
+CREATE TABLE Locations (PrimaryKey SERIAL PRIMARY KEY,
+                        MemberID INT,
+                        Nickname VARCHAR(255),
+                        Lat DECIMAL,
+                        Long DECIMAL,
+                        ZIP INT,
+                        FOREIGN KEY(MemberID) REFERENCES Members(MemberID)
+);
+
+DROP TABLE IF EXISTS Demo;
+CREATE TABLE Demo (DemoID SERIAL PRIMARY KEY,
+                        Text VARCHAR(255)
+);
+
+
+DROP TABLE IF EXISTS Push_Token;
+CREATE TABLE Push_Token (KeyID SERIAL PRIMARY KEY,
+                        MemberID INT NOT NULL UNIQUE,
+                        Token VARCHAR(255),
+                        FOREIGN KEY(MemberID) REFERENCES Members(MemberID)
+);
---
+++ index.js
@@ -0,0 +1,59 @@
+//express is the framework we're going to use to handle requests
+const express = require('express');
+//Create a new instance of express
+const app = express();
+
+//let middleware = require('./utilities/middleware');
+
+const bodyParser = require("body-parser");
+//This allows parsing of the body of POST requests, that are encoded in JSON
+app.use(bodyParser.json());
+
+//pg-promise is a postgres library that uses javascript promises
+const pgp = require('pg-promise')();
+//We have to set ssl usage to true for Heroku to accept our connection
+pgp.pg.defaults.ssl = true;
+
+//Create connection to Heroku Database
+let db = pgp(process.env.DATABASE_URL);
+
+if(!db) {
+   console.log("SHAME! Follow the intructions and set your DATABASE_URL correctly");
+   process.exit(1);
+}
+
+
+
+
+
+
+
+
+/*
+ * Return HTML for the / end point. 
+ * This is a nice location to document your web service API
+ * Create a web page in HTML/CSS and have this end point return it. 
+ * Look up the node module 'fs' ex: require('fs');
+ */
+app.get("/", (req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    for (i = 1; i < 7; i++) {
+        //write a response to the client
+        res.write('<h' + i + ' style="color:blue">Hello World!</h' + i + '>'); 
+    }
+    res.end(); //end the response
+});
+
+/* 
+* Heroku will assign a port you can use via the 'PORT' environment variable
+* To accesss an environment variable, use process.env.<ENV>
+* If there isn't an environment variable, process.env.PORT will be null (or undefined)
+* If a value is 'falsy', i.e. null or undefined, javascript will evaluate the rest of the 'or'
+* In this case, we assign the port to be 5000 if the PORT variable isn't set
+* You can consider 'let port = process.env.PORT || 5000' to be equivalent to:
+* let port; = process.env.PORT;
+* if(port == null) {port = 5000} 
+*/ 
+app.listen(process.env.PORT || 5000, () => {
+    console.log("Server up and running on port: " + (process.env.PORT || 5000));
+});
