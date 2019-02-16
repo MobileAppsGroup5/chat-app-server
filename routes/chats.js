9cc31878c7cf2b3690981e144610aa3ccfedfbe3
Michael Osborne <resonatortune@gmail.com>
2019-02-16 14:17:49 -0800

    Chats endpoint added with /new and /getChats

---
+++ routes/chats.js
@@ -0,0 +1,96 @@
+//express is the framework we're going to use to handle requests
+const express = require('express');
+//Create connection to Heroku Database
+let db = require('../utilities/utils').db;
+var router = express.Router();
+const bodyParser = require("body-parser");
+//This allows parsing of the body of POST requests, that are encoded in JSON
+router.use(bodyParser.json());
+// Create a new chat
+router.post("/new", (req, res) => {
+  let username1 = req.body['username1'];
+  let username2 = req.body['username2'];
+  let chatName = req.body['chatName'];
+  if (!username1 || !username2 || !chatName) {
+    res.send({
+      success: false,
+      error: "one or all of username1, username2, or chatName were not supplied"
+    });
+    return;
+  }
+
+  // Get member ids for the given usernames
+  console.log(username1 + username2);
+  db.many(`SELECT memberid FROM members WHERE username=$1 OR username=$2`, [username1, username2])
+    .then((rows) => {
+      // Insert chat into chats
+      db.one(`INSERT INTO Chats(Name) VALUES ($1) RETURNING chatid`, [chatName])
+        .then((row) => {
+          console.log(row);
+          // Assign the two people to chatMembers of the chat
+          db.none(`INSERT INTO ChatMembers(ChatID, MemberID) VALUES ($1, $2), ($1, $3)`, [row.chatid, rows[0].memberid, rows[1].memberid])
+            .then(() => {
+              res.send({
+                success: true,
+                message: 'Chat room successfully set up!',
+              })
+              return;
+            }).catch((err) => {
+              res.send({
+                success: false,
+                error: 'Failed to assign users to chatroom ' + err,
+              });
+              return;
+            })
+        }).catch((err) => {
+          res.send({
+            success: false,
+            error: 'Failed to create new chat ' + err,
+          });
+          return;
+        });
+    }).catch((err) => {
+      res.send({
+        success: false,
+        error: 'One or both usernames do not exist ' + err,
+      });
+      return;
+    })
+  // add the chat to the chat server
+
+});
+// get all chat rooms
+router.get("/getChats", (req, res) => {
+  let username = req.body['username'];
+  if (!username) {
+    res.send({
+      success: false,
+      error: "username not supplied"
+    });
+    return;
+  }
+
+  // find the memberid for the given username
+  db.one('SELECT memberid FROM members WHERE username=$1', [username])
+    .then((row) => {
+      // Get all associated chat ids
+      console.log(row)
+      db.manyOrNone(`SELECT chatmembers.chatId, chats.name FROM chatmembers INNER JOIN chats ON chatmembers.chatid=chats.chatid WHERE memberid=$1`, [row.memberid])
+        .then((rows) => {
+          res.send({
+            messages: rows
+          })
+        }).catch((err) => {
+          res.send({
+            success: false,
+            error: 'Failed to fetch chat rooms for user ' + err
+          })
+        });
+    }).catch((err) => {
+      res.send({
+        success: false,
+        error: 'User does not exist ' + err
+      })
+    })
+});
+module.exports = router;
--- routes/messaging.js
+++ routes/messaging.js
@@ -9,10 +9,10 @@ router.use(bodyParser.json());
 let msg_functions = require('../utilities/utils').messaging;
 //send a message to all users "in" the chat session with chatId
 router.post("/send", (req, res) => {
-  let email = req.body['email'];
+  let username = req.body['username'];
   let message = req.body['message'];
   let chatId = req.body['chatId'];
-  if (!email || !message || !chatId) {
+  if (!username || !message || !chatId) {
     res.send({
       success: false,
       error: "email, message, or chatId not supplied"
@@ -20,14 +20,14 @@ router.post("/send", (req, res) => {
     return;
   }
   //add the message to the database
-  let insert = `INSERT INTO Messages(ChatId, Message, MemberId) SELECT $1, $2, MemberId FROM Members WHERE email=$3`
-  db.none(insert, [chatId, message, email])
+  let insert = `INSERT INTO Messages(ChatId, Message, MemberId) SELECT $1, $2, MemberId FROM Members WHERE username=$3`
+  db.none(insert, [chatId, message, username])
     .then(() => {
       //send a notification of this message to ALL members with registered tokens
       db.manyOrNone('SELECT * FROM Push_Token')
         .then(rows => {
           rows.forEach(element => {
-            msg_functions.sendToIndividual(element['token'], message, email);
+            msg_functions.sendToIndividual(element['token'], message, username);
           });
           res.send({
             success: true
@@ -48,8 +48,8 @@ router.post("/send", (req, res) => {
 //Get all of the messages from a chat session with id chatid
 router.post("/getAll", (req, res) => {
   let chatId = req.body['chatId'];
-  
-  let query = `SELECT Members.Email, Messages.Message, to_char(Messages.Timestamp AT TIME ZONE 'PDT', 'YYYY-MM-DD HH24:MI:SS.US' ) AS Timestamp FROM Messages INNER JOIN Members ON Messages.MemberId=Members.MemberId WHERE ChatId=$1 ORDER BY Timestamp DESC`;
+
+  let query = `SELECT Members.username, Messages.Message, to_char(Messages.Timestamp AT TIME ZONE 'PDT', 'YYYY-MM-DD HH24:MI:SS.US' ) AS Timestamp FROM Messages INNER JOIN Members ON Messages.MemberId=Members.MemberId WHERE ChatId=$1 ORDER BY Timestamp DESC`;
   db.manyOrNone(query, [chatId])
     .then((rows) => {
       res.send({
