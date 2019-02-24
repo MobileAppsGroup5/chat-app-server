905e17d9a0b463d4d77b9c2261f631dc94891180
Michael Osborne <resonatortune@gmail.com>
2019-02-23 19:03:15 -0800

    Tweak email html

--- utilities/email.js
+++ utilities/email.js
@@ -19,11 +19,7 @@ function sendVerificationEmail(email) {
       // <a href=\'' + user_verify_url + '\'>' + user_verify_url + '</a>';
 
       let email_body = '<!DOCTYPE html><html><title>Verify your account! </title>\
-      <head><meta name="viewport" content="width=device-width, initial-scale=1">\
-      <style>p {text-align: center;}h1 {text-align: center;font-family: Verdana, Geneva, Tahoma, sans-serif;}\
-      body {background-image: url(\'https://i.imgur.com/KfRqtQp.png\');background-repeat: no-repeat;background-size: \
-      contain;background-position: center; background-color:powderblue;}\
-      img {width: auto;max-width: 100%;height: auto; }</style></head>\
+      <head><meta name="viewport" content="width=device-width, initial-scale=1"></head>\
       <img style="display:block;margin-left:auto;margin-right:auto;width:200px;height:200px;" src="https://i.imgur.com/KfRqtQp.png"/>\
       <body bgcolor="#212121" text="white"><h1 style="font-size: 16px;text-align: center;font-family: Verdana, Geneva, Tahoma, sans-serif;">Welcome Chappsters!</h1><br><p style="text-align: center;">Dear Fellow Chappster,</p>\
       <p style="text-align: center;">Thanks for registering with CHAPP "The Best Chat App". \
